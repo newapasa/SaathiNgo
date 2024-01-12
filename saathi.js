a741ffbe070848e9c34ed1172ee836bf1eb598f8
Shreejan_Prajapati <newapasa7@gmail.com>
2024-01-12 19:42:18 +0545

    New site

---
+++ saathi.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+
+$(".nextpage ul li a").click(function(){
+    $(".nextpage ul li a").removeClass("active");
+    $(this).addClass("active");
+})
